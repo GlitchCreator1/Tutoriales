9d9ba9f507a5319fdf8ff38526a4f2de0c9a7d17
Glitch Creator <rherrdz11@gmail.com>
2024-11-16 09:15:51 -0600

    Update 1_Variables.lua

--- Aprendizaje/1_Variables.lua
+++ Aprendizaje/1_Variables.lua
@@ -1,7 +1,7 @@
 -- Una variable es un espacio de memoria que se utiliza para almacenar y manipular datos. 
 -- Puede ser accedida y modificada desde cualquier parte del código, siempre y cuando esté en el mismo ámbito o alcance.
 
-nombre = "Buben" -- VARIABLE TIPO TEXTO
+nombre = "Glitch" -- VARIABLE TIPO TEXTO
 edad = "999" -- VARIABLE NUMERICA
 suscriptores = 16 -- VARIABLE NUMERICA
 
@@ -9,14 +9,15 @@ suscriptores = 16 -- VARIABLE NUMERICA
 
 print("Se llama", nombre, "su edad es", edad, "años, y tiene", suscriptores, "suscriptores")
 
--- Este texto aparecera en la consola como:  "Se llama Buben su edad es 999 años, y tiene 16 suscriptores"
+-- Este texto aparecera en la consola como:  "Se llama Glitch su edad es 999 años, y tiene 16 suscriptores"
 
 -- Nota: Esta seccion del GitHub es solo APRENDIZAJE, no te recomiendo utilizar estos scripts a menos de que sea necesario,
 -- pero  si quieres utilizarlos, adelante! :D.  Si tienes alguna duda enviamela en los comentarios del canal.
 
---██████╗ ██╗   ██╗       ██████╗ ██╗   ██╗██████╗ ███████╗███╗   ██╗███████╗
---██╔══██╗╚██╗ ██╔╝██╗    ██╔══██╗██║   ██║██╔══██╗██╔════╝████╗  ██║██╔════╝
---██████╔╝ ╚████╔╝ ╚═╝    ██████╔╝██║   ██║██████╔╝█████╗  ██╔██╗ ██║███████╗ si lees esto y eres iker
---██╔══██╗  ╚██╔╝  ██╗    ██╔══██╗██║   ██║██╔══██╗██╔══╝  ██║╚██╗██║╚════██║ no pongas bolsas en la
---██████╔╝   ██║   ╚═╝    ██████╔╝╚██████╔╝██████╔╝███████╗██║ ╚████║███████║ cabeza
---╚═════╝    ╚═╝          ╚═════╝  ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝
+-- ██████╗ ██╗     ██╗████████╗ ██████╗██╗  ██╗     ██████╗██████╗ ███████╗ █████╗ ████████╗ ██████╗ ██████╗ 
+--██╔════╝ ██║     ██║╚══██╔══╝██╔════╝██║  ██║    ██╔════╝██╔══██╗██╔════╝██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗
+--██║  ███╗██║     ██║   ██║   ██║     ███████║    ██║     ██████╔╝█████╗  ███████║   ██║   ██║   ██║██████╔╝
+--██║   ██║██║     ██║   ██║   ██║     ██╔══██║    ██║     ██╔══██╗██╔══╝  ██╔══██║   ██║   ██║   ██║██╔══██╗
+--╚██████╔╝███████╗██║   ██║   ╚██████╗██║  ██║    ╚██████╗██║  ██║███████╗██║  ██║   ██║   ╚██████╔╝██║  ██║
+-- ╚═════╝ ╚══════╝╚═╝   ╚═╝    ╚═════╝╚═╝  ╚═╝     ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
+                                                                                                           
