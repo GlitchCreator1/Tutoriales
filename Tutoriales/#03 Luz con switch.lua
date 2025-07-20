ea35ee465d3d3e987e76e3997a2bb21e0e92af7c
Glitch Creator <rherrdz11@gmail.com>
2025-07-19 20:12:32 -0600

    Update and rename #02 Luz con switch.lua to #03 Luz con switch.lua
    Actualización nuevo sistema

--- Tutoriales/#03 Luz con switch.lua
+++ Tutoriales/#03 Luz con switch.lua
@@ -1,41 +1,44 @@
----> Variables
+---> Variables <---
 
-local Module = script.Parent
-local Light = Module:WaitForChild("Light"):WaitForChild("Light")
-local LightPoint = Light:WaitForChild("PointLight")
-local Switch = Module:WaitForChild("LightSwitch")
-local Switch1 = Switch:WaitForChild("SWIS")
-local click = Switch:WaitForChild("ClickDetector")
-local Sound = Switch:WaitForChild("Sound")
-local Switch = false
+local folder = script.Parent
 
----> Funciones
+local switch = folder.Switch ---> Cambia a tu switch
+local clickdetector = switch.Switch.ClickDetector ---> Cambia al click detector de tu switch
 
-click.MouseClick:Connect(function()
-	
-	if not Switch then
-		Switch = true
-		Sound:Play()
-		LightPoint.Enabled = true
-		Light.Material = "Neon"
-		Light.Color = Color3.new(1, 1, 1)
-		Switch1.Orientation = Vector3.new(0, -90, 0)
-		Switch1.Position = Vector3.new(112.904, 4.664, -58.591)
-		
-		
-	else
-		Switch = false
-		Sound:Play()
-		LightPoint.Enabled = false
-		Light.Material = "Glass"
-		Light.Color = Color3.new(0.623529, 0.631373, 0.67451)
-		Switch1.Orientation = Vector3.new(0, -90, 72)
-		Switch1.Position = Vector3.new(112.904, 4.508, -58.607)
-		
+local lightsfolder = folder.Lights
+local lights = lightsfolder:GetChildren()
+
+local luces = false
+
+---> Funcion <---
+
+local function AlternarLuces()
+	if lightsfolder then
+		if #lights > 0 then
+			luces = not luces
+			
+			for i, hijo in pairs(lights) do
+				if not luces then
+					if hijo.Name == "Light" then
+						hijo.Light.Material = Enum.Material.Neon ---> Puedes cambiar el material
+						hijo.Light.PointLight.Enabled = true
+					end
+				else
+					if hijo.Name == "Light" then
+						hijo.Light.Material = Enum.Material.Plastic ---> ---> Puedes cambiar el material
+						hijo.Light.PointLight.Enabled = false
+					end
+				end
+			end
+		end
 	end
-	
-end)
+end
 
+---> Evento <---
+
+clickdetector.MouseClick:Connect(AlternarLuces)
+
+---> No se recomienda cambiar el script a menos que el video lo indique, o tengas experiencia, el script es sensible a errores si no esta bien programado. <---
 
 -- ██████╗ ██╗     ██╗████████╗ ██████╗██╗  ██╗     ██████╗██████╗ ███████╗ █████╗ ████████╗ ██████╗ ██████╗ 
 --██╔════╝ ██║     ██║╚══██╔══╝██╔════╝██║  ██║    ██╔════╝██╔══██╗██╔════╝██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗
